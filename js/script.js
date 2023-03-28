7d34afbd8f61c6dafee129f5f6ae38d8edc3e29b
PedroPenha <pedro.eng98@gmail.com>
2023-03-28 17:40:20 -0300

    concluded

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@ searchIcon.addEventListener('click', () => {
     searchBox.classList.toggle('active')
 })
 
+//Header
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+})
